1dbe028299805a9bbf687c185acab3953bdd2e67
Minh <elevenine00@gmail.com>
2024-06-19 17:11:46 +0700

    feat(new): introduce PPR

--- src/photo/db/query.ts
+++ src/photo/db/query.ts
@@ -23,6 +23,7 @@ import {
 import { getWheresFromOptions } from '.';
 import { FocalLengths } from '@/focal';
 import { Lenses, createLensKey } from '@/lens';
+import { screenForPPR } from '@/utility/prr';
 
 const createPhotosTable = () =>
   sql`
@@ -89,6 +90,7 @@ const safelyQueryPhotos = async <T>(
   try {
     result = await callback();
   } catch (e: any) {
+    screenForPPR(e, undefined, 'neon postgres');
     if (MIGRATION_FIELDS_01.some(field => new RegExp(
       `column "${field}" of relation "photos" does not exist`,
       'i',
@@ -311,7 +313,7 @@ export const getUniqueLenses = async () =>
         lens: { make, model },
         count: parseInt(count, 10),
       })))
-  , 'getUniqueCameras');
+  , 'getUniqueLenses');
 
 export const getUniqueFilmSimulations = async () =>
   safelyQueryPhotos(() => sql`
