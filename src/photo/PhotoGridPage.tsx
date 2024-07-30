cdf0884704a7542fc9cf525393b11b883cb371b9
Minh <elevenine00@gmail.com>
2024-07-30 11:40:33 +0700

    debug grid page unmount

--- src/photo/PhotoGridPage.tsx
+++ src/photo/PhotoGridPage.tsx
@@ -30,7 +30,10 @@ export default function PhotoGridPage({
   const { setSelectedPhotoIds } = useAppState();
   
   useEffect(
-    () => () => setSelectedPhotoIds?.(undefined),
+    () => {
+      console.log('PhotoGridPage: unmount');
+      return () => setSelectedPhotoIds?.(undefined);
+    },
     [setSelectedPhotoIds]
   );
 
