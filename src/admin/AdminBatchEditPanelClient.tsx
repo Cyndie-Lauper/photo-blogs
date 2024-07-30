756959e742ad21a80025feab5dc05fd7cec9b70c
Minh <elevenine00@gmail.com>
2024-07-30 13:36:14 +0700

    add basic tag validation

--- src/admin/AdminBatchEditPanelClient.tsx
+++ src/admin/AdminBatchEditPanelClient.tsx
@@ -9,7 +9,8 @@ import { IoCloseSharp } from 'react-icons/io5';
 import DeleteButton from './DeleteButton';
 import { useState } from 'react';
 import TagInput from '@/components/TagInput';
-import { convertTagsForForm, TagsWithMeta } from '@/tag';
+// eslint-disable-next-line max-len
+import { convertTagsForForm, getValidationMessageForTags, TagsWithMeta } from '@/tag';
 import { usePathname } from 'next/navigation';
 import { PATH_GRID_INFERRED } from '@/site/paths';
 
@@ -26,6 +27,7 @@ export default function AdminBatchEditPanelClient({
   } = useAppState();
 
   const [tags, setTags] = useState<string>();
+  const tagValidationMessage = getValidationMessageForTags(tags);
   const isTagging = tags !== undefined;
 
   const photosPlural = selectedPhotoIds?.length === 1 ? 'photo' : 'photos';
@@ -46,6 +48,7 @@ export default function AdminBatchEditPanelClient({
         className="min-h-[2.5rem]"
         // eslint-disable-next-line max-len
         confirmText={`Are you sure you want to apply tags to ${selectedPhotoIds?.length} ${photosPlural}? This action cannot be undone.`}
+        disabled={!tags || Boolean(tagValidationMessage)}
         primary
       >
         Apply Tags
@@ -93,6 +96,9 @@ export default function AdminBatchEditPanelClient({
             options={convertTagsForForm(existingTags)}
             onChange={setTags}
             placeholder={`Tag ${selectedPhotoIds?.length} ${photosPlural} ...`}
+            className={clsx(
+              Boolean(tagValidationMessage) && 'error',
+            )}
           />
           : <div className="text-base">
             {renderPhotoText()}
